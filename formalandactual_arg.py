9a57bc24431e1e933354ef0c621454bb054af982
PrasadMallampati <107248009+prasadmallampati@users.noreply.github.com>
2024-07-02 17:25:18 +0530

    Add files via upload

---
+++ formalandactual_arg.py
@@ -0,0 +1,4 @@
+# actual or formal argments
+def add(a,b):
+    return a+b
+add(10,20)# actual argments
---
+++ positinal_arg.py
@@ -0,0 +1,6 @@
+# positinal arguments
+
+
+def rect(w,h):
+    return w*h
+print(rect(w=10,h=20))
